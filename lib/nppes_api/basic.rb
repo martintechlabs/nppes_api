a2a2f2e2b0bea0f9639ba35460710e8dba34ce12
Stephen Martin <stephen@martintechlabs.com>
2018-08-02 22:03:19 -0400

    adding authorized_official info

--- lib/nppes_api/basic.rb
+++ lib/nppes_api/basic.rb
@@ -15,7 +15,13 @@ module NPPESApi
       :gender,
       :sole_proprietor,
       :last_updated,
-      :enumeration_date
+      :enumeration_date,
+      :authorized_official_first_name,
+      :authorized_official_middle_name,
+      :authorized_official_last_name,
+      :authorized_official_credential,
+      :authorized_official_title_or_position,
+      :authorized_official_telephone_number
     ]
 
     methods.each do |meth|
